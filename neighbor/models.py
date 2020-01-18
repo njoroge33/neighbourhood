135456cf36eacb3ec1a17c5674ee68621fbbd0ff
njoroge33 <johngichuhi769@gmail.com>
2020-01-18 22:56:24 +0300

    create profile model

--- neighbor/models.py
+++ neighbor/models.py
@@ -7,9 +7,29 @@ class Neighborhood(models.Model):
     location = models.CharField(max_length = 50)
     total_occupants = models.IntegerField()
     admin = models.ForeignKey(User, on_delete = models.CASCADE)
+    health_contacts = models.CharField(max_length = 50)
+    police_contacts = models.CharField(max_length = 50)
     
     def create_neighborhood():
         self.save()
 
     def delete_neighborhood():
         self.delete()
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE,)
+    name = models.CharField(max_length = 100)
+    national_id = models.CharField(max_length = 9)
+    neighborhood = models.ForeignKey(Neighborhood, on_delete = models.CASCADE)
+
+    def save_profile(self):
+        self.save()
+
+    def delete_profile(self):
+        self.delete()
+
+    def updateProfile(sender, **kwargs):
+        if kwargs['created']:
+            profile = Profile.objects.created(user=kwargs['instance'])
+
+            post_save.connect(Profile, sender=User)
