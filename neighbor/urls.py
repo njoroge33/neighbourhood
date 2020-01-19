e59825644d0b00e098006020a02c0612bd3e18d4
njoroge33 <johngichuhi769@gmail.com>
2020-01-19 14:46:20 +0300

    implent logout functionality

--- neighbor/urls.py
+++ neighbor/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, re_path
 from . import views
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, LogoutView
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -8,6 +8,7 @@ urlpatterns = [
     path('home', views.home, name='home'),
     path('', views.signup, name='signup'),
     re_path(r'login/', LoginView.as_view(), name='login'),
+    re_path(r'logout/', LogoutView.as_view(next_page='login'), name='logout'),
     re_path(r'^profile/', views.profile, name='profile'),
     re_path(r'^updateprofile/', views.update_profile, name='update_profile'),
 ]
